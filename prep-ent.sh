5317f236d1fecf79b0ecca7cff01e7294a5c3515
bbkmg256 <tecded@proton.me>
2024-08-31 21:53:14 -0300

    Creacion fichero de preparacion de entorno (testearlo)

---
+++ prep-ent.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo -e " * Preparando entorno...\n"
+sleep 2
+
+# Si binutils y python3-venv no están instalados
+if [[ $(sudo dpkg --get-selections | grep binutils) == "" && $(sudo dpkg --get-selections | grep python3-venv) == "" ]]; then
+	sudo apt update
+	sudo apt install binutils python3-venv
+fi
+
+# Si no se encuentra el dir ".venv"
+if [ $(ls .venv &> /dev/null; echo $?) -gt 0 ]; then
+	python3 -m venv .venv
+fi
+
+# Ingresa en entorno virtual
+source .venv/bin/activate
+pip3 install -r requirements.txt
+echo -e " * Entorno preparado!\n"
+echo -e " * Ejecute 'source .venv/bin/activate' para entrar en el entorno virtual.\n"
+exit 0
